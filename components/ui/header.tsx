0e8fa8326cc3b10f197a70c371f7ab57b7312e2b
KebabOnline <merek77777@gmail.com>
2025-04-06 23:48:04 +0200

    added mobile header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -8,8 +8,17 @@ export default function Header() {
 
     return (
         <div className="flex items-center w-full size-7">
-            {!open && <SidebarTrigger />}
-            <h1>Kebab.ws</h1>
+            {/* Mobile Header */}
+            <div className="flex md:hidden items-center w-full">
+                <SidebarTrigger />
+                <h1 className="absolute left-1/2 transform -translate-x-1/2">Kebab.ws</h1> {/* Center h1 on mobile */}
+            </div>
+
+            {/* Desktop Header */}
+            <div className="hidden md:flex items-center">
+                {!open && <SidebarTrigger />} {/* Only show header when sidebar is closed*/}
+                <h1>Kebab.ws</h1>
+            </div>
         </div>
     );
 }
