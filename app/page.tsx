565a1d95c0948d913ac6c45323f88f21479c895d
KebabOnline <merek77777@gmail.com>
2025-05-04 17:12:10 +0200

    new gif

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="flex flex-col items-center justify-center p-24">
         <h1 className="text-4xl font-bold">Kebab.ws</h1>
-        <img className="w-50 h-auto m-4" src="https://i.pinimg.com/736x/9c/09/cc/9c09cc83a8b2fe8d8a857958632c00c2.jpg"/>
+        <img className="m-4" src="https://i.pinimg.com/originals/97/2f/54/972f5458055bb7a93ffa87b446cc7427.gif"/>
         <p className="text-lg">under construction</p>
       </div>
     </main>
